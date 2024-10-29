7688a6d613dcce89652abe2d7a0d8daa6353f9dc
AleksBerkhald <berkhald.aleks@gmail.com>
2024-10-29 11:25:50 +0200

    failed build files, reversed back

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,8 @@
-#!/bin/bash
+echo -m pip install -r requirements.txt
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Install Python dependencies
-echo "Installing dependencies from requirements.txt"
-pip install -r requirements.txt
-
-# Run Django migrations
-echo "Running migrations"
+echo
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
-# Collect static files
-echo "Collecting static files"
+echo
 python3 manage.py collectstatic --noinput
